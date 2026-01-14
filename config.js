40f1a42e61df33bd5924d2981542784a8b7a28f7
aavonsamuel-alt <aavon.samuel@gmail.com>
2026-01-14 16:00:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me fr?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "Nah",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "Neither, I LOVE you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -43,13 +43,13 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        high: "I love you lots! 🤗💝",              // Shows when they go past 1000%
+        normal: "Maybe a lil bit🤏🏾"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "YAYYYYY!!! I'm the luckiest guy in the WORLD🙂‍↕️🥰❤️",
+        title: "I'm the luckiest guy in the WORLD🙂‍↕️🥰❤️",
         message: "I can't wait to celebrate my FIRST VALENTINE'S DAY with you my baby!🥰",
         emojis: "🎁💖🤗💝❤️💕"  // These will bounce around
     },
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "content://media/external/downloads/1000003201", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
